fca6afa1ba6c69e8c180b137ab147518ed76f35a
Kirik-hamster <mkir2@bk.ru>
2024-04-11 23:14:11 +0400

    добавил скрипт sql

--- sql.sql
+++ sql.sql
@@ -5,5 +5,6 @@ CREATE TABLE t_stat (
     C_path varchar(255), 
     C_size int, 
     C_elapsed_time int, 
-    C_date timestamp 
+    C_date timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    C_size_string varchar(255)
 );
