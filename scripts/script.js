f4123364bbcc1a8edaf6d1a490e2379abd6f8e62
Kirik-hamster <mkir2@k.ru>
2024-04-12 09:50:00 +0400

    отрисовка графика в отделльной файле

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+import { drawGraf } from "./drowGraf.js";
 async function fetchJson() {
     const url = new URL("http://localhost/Statistic/get-statistic.php");
     try {
@@ -20,65 +21,7 @@ async function fetchJson() {
 document.addEventListener('DOMContentLoaded', async () => {
     const jsonData = await fetchJson();
 
-    let data = jsonData;
-
-    let labels = data.map((i) => {
-        return i.C_size_string;
-    });
-
-    let size = data.map((i) => {
-        return 'item' + i.C_size;
-    });
-
-    let elapsedTime = data.map((i) => {
-        return i.C_elapsed_time;
-    });
-    console.log(elapsedTime)
-
-    let config = {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                    label: 'Elapsed time',
-                    data: elapsedTime,
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)', 
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    type: 'linear',
-                    position: 'left',
-                    ticks: {
-                        callback: function(value, index, values) {
-                            let unit = 'ns'; 
-                            let convertedValue = value;
-
-                            if (value >= 1e9) {
-                                unit = 's';
-                                convertedValue = value / 1e9;
-                            } else if (value >= 1e6) {
-                                unit = 'ms';
-                                convertedValue = value / 1e6;
-                            } else if (value >= 1e3) {
-                                unit = 'µs';
-                                convertedValue = value / 1e3;
-                            }
-
-                            return convertedValue.toFixed(1) + ' ' + unit;
-                        }
-                    }
-                }
-            }
-        }
-    };
-
-    let ctx = document.getElementById('myChart').getContext('2d');
-
-    var myChart = new Chart(ctx, config);
+    drawGraf(jsonData);
 
     let table = document.querySelector("table");
     for (let i = 0; i < jsonData.length; i++) {
